23925ab2cdc4f5d32a30497a5a0b633793cddce0
Jose Ramon Bogarin <bogarin617@gmail.com>
2015-10-02 09:41:10 -0700

    codigo semi completo

--- Masajista.java
+++ Masajista.java
@@ -1,5 +1,19 @@
 package polimorfismoAbstraccion;
 
-public class Masajista {
+public class Masajista extends Seleccionfutbol {
+
+	   private String titulacion;
+	   private int aniosExperiencia;
+
+	   // constructor, getter y setter
+		
+	   @Override
+	   public void entrenamiento() {
+	      System.out.println("Da asistencia en el entrenamiento (Clase Masajista)");
+	   }
+
+	   public void darMasaje() {
+	      System.out.println("Da un Masaje");
+	   }
 
 }
--- Principal.java
+++ Principal.java
@@ -2,9 +2,33 @@ package polimorfismoAbstraccion;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static ArrayList<Seleccionfutbol> integrantes = new ArrayList<Seleccionfutbol>();
 
-	}
+	public static void main(String[] args) {
+		
+		Seleccionfutbol delBosque = new Entrenador(1, "Vicente", "Del Bosque", 60, 28489);
+		Seleccionfutbol iniesta = new Futbolista(2, "Andres", "Iniesta", 29, 6, "Interior Derecho");
+		Seleccionfutbol raulMartinez = new Masajista(3, "Raúl", "Martinez", 41, "Licenciado en Fisioterapia", 18);
+
+		integrantes.add(delBosque);
+		integrantes.add(iniesta);
+		integrantes.add(raulMartinez);
+
+		// CONCENTRACION
+		System.out.println("Todos los integrantes comienzan una concentracion. (Todos ejecutan el mismo método)");
+		for (Seleccionfutbol integrante : integrantes) {
+			System.out.print(integrante.getNombre() + " " + integrante.getApellidos() + " -> ");
+			integrante.concentrarse();
+		}
+
+		// VIAJE
+		System.out.println("nTodos los integrantes viajan para jugar un partido. (Todos ejecutan el mismo método)");
+		for (Seleccionfutbol integrante : integrantes) {
+			System.out.print(integrante.getNombre() + " " + integrante.getApellidos() + " -> ");
+			integrante.viajar();
+		}
+
+      
+}
 
 }
